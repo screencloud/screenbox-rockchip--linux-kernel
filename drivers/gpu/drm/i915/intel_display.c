9064d280bd86af8fb1871e10da53086ce205f5d5
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2016-01-28 15:04:58 +0100

    UPSTREAM: drm/atomic: Add encoder_mask to crtc_state, v3.
    This allows iteration over encoders without requiring connection_mutex.
    
    Changes since v1:
    - Add a set_best_encoder helper function and update encoder_mask inside
      it.
    Changes since v2:
    - Relax the WARN_ON(!crtc), with explanation.
    - Call set_best_encoder when connector is moved between crtc's.
    - Add some paranoia to steal_encoder to prevent accidentally setting
      best_encoder to NULL.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/56AA200A.6070501@linux.intel.com
    (cherry picked from commit e87a52b38943608531ff63ea397b1ae5dd48e341
    from v4.6-rc1)
    Signed-off-by: Tarun Vyas <tarun.vyas@intel.com>
    
    BUG=chrome-os-partner:52640
    TEST=Image builds, boots and is browse-able
    
    Change-Id: Iddfce503c59ed4f771920aefff108596067fad05
    Reviewed-on: https://chromium-review.googlesource.com/343109
    Commit-Ready: Tarun Vyas <tarun.vyas@intel.com>
    Tested-by: Russ Sage <russ.sage@intel.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -15152,7 +15152,6 @@ static void intel_sanitize_crtc(struct intel_crtc *crtc)
 		crtc->base.enabled = crtc->active;
 
 		/* Because we only establish the connector -> encoder ->
-		 * crtc links if something is active, this means the
 		 * crtc is now deactivated. Break the links. connector
 		 * -> encoder links are only establish when things are
 		 *  actually up, hence no need to break them. */
@@ -15352,7 +15351,23 @@ static void intel_modeset_readout_hw_state(struct drm_device *dev)
 	for_each_intel_connector(dev, connector) {
 		if (connector->get_hw_state(connector)) {
 			connector->base.dpms = DRM_MODE_DPMS_ON;
-			connector->base.encoder = &connector->encoder->base;
+
+			encoder = connector->encoder;
+			connector->base.encoder = &encoder->base;
+
+			if (encoder->base.crtc &&
+			    encoder->base.crtc->state->active) {
+				/*
+				 * This has to be done during hardware readout
+				 * because anything calling .crtc_disable may
+				 * rely on the connector_mask being accurate.
+				 */
+				encoder->base.crtc->state->connector_mask |=
+					1 << drm_connector_index(&connector->base);
+				encoder->base.crtc->state->encoder_mask |=
+					1 << drm_encoder_index(&encoder->base);
+			}
+
 		} else {
 			connector->base.dpms = DRM_MODE_DPMS_OFF;
 			connector->base.encoder = NULL;
