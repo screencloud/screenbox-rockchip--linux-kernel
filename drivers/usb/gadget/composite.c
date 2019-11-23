edf95db1d177e4e44eede134d80debd80dad506a
Kamil Trzciński <ayufan@ayufan.eu>
2019-11-23 14:01:40 +0100

    PATCH: kernel 4.4.200-201

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2078,6 +2078,7 @@ void composite_dev_cleanup(struct usb_composite_dev *cdev)
 			usb_ep_dequeue(cdev->gadget->ep0, cdev->os_desc_req);
 
 		kfree(cdev->os_desc_req->buf);
+		cdev->os_desc_req->buf = NULL;
 		usb_ep_free_request(cdev->gadget->ep0, cdev->os_desc_req);
 		cdev->os_desc_req = NULL;
 	}
@@ -2086,6 +2087,7 @@ void composite_dev_cleanup(struct usb_composite_dev *cdev)
 			usb_ep_dequeue(cdev->gadget->ep0, cdev->req);
 
 		kfree(cdev->req->buf);
+		cdev->req->buf = NULL;
 		usb_ep_free_request(cdev->gadget->ep0, cdev->req);
 		cdev->req = NULL;
 	}
